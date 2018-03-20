25698268a0eeecd284ae6e2a45d74b079b949f7b
Taylor Lassiter <lassit09@gmail.com>
2018-03-19 21:46:00 -0400

    Define 'campgrounds' array with names and images

--- v1/app.js
+++ v1/app.js
@@ -9,6 +9,15 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
+//campgrounds page
+app.get("/campgrounds", function(req, res) {
+    var campgrounds = [
+        {name: "Salmon Creek", image: "https://www.flickr.com/photos/yellowstonenps/33668700361/"}
+        {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8aROYLPJmJXqDOlCRb5MAwSrUODid-cCOBZ2gy6LClH9gvxoI"}
+        {name: "Mountain Goat's Rest", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqJU8kuRKQ9SzP-XwuupkYAM910WgyR3o3V3MB5adzCr0xk0vF"}
+    ];   
+});
+
 //catchall page
 app.get("*", function(req, res) {
     res.send("THIS PAGE COULD NOT BE FOUND");
