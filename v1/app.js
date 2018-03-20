55250982c86566df32cab27c226e58dff3b3ed28
Taylor Lassiter <lassit09@gmail.com>
2018-03-19 21:51:55 -0400

    Add render of 'campgrounds' page to 'campgrounds' route, create campgrounds.ejs and add heading to it

--- v1/app.js
+++ v1/app.js
@@ -12,10 +12,12 @@ app.get("/", function(req, res) {
 //campgrounds page
 app.get("/campgrounds", function(req, res) {
     var campgrounds = [
-        {name: "Salmon Creek", image: "https://www.flickr.com/photos/yellowstonenps/33668700361/"}
-        {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8aROYLPJmJXqDOlCRb5MAwSrUODid-cCOBZ2gy6LClH9gvxoI"}
+        {name: "Salmon Creek", image: "https://www.flickr.com/photos/yellowstonenps/33668700361/"},
+        {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8aROYLPJmJXqDOlCRb5MAwSrUODid-cCOBZ2gy6LClH9gvxoI"},
         {name: "Mountain Goat's Rest", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqJU8kuRKQ9SzP-XwuupkYAM910WgyR3o3V3MB5adzCr0xk0vF"}
-    ];   
+    ]
+    
+    res.render("campgrounds");
 });
 
 //catchall page
