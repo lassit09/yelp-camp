e4ad185326b95a016e4eda20a6fe8aca88430884
Taylor Lassiter <lassit09@gmail.com>
2018-03-19 21:30:09 -0400

    Add catchall route

--- v1/app.js
+++ v1/app.js
@@ -7,6 +7,11 @@ app.get("/", function(req, res) {
     res.send("THIS IS THE HOMEPAGE");
 });
 
+//catchall page
+app.get("*", function(req, res) {
+    res.send("THIS PAGE COULD NOT BE FOUND");
+});
+
 //make server listen
 app.listen(3000, function() {
     console.log("The YelpCamp Server Has Started");
