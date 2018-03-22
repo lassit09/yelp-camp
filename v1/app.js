0bd76c489790681065d3851bab48a09f7549c4df
Taylor Lassiter <lassit09@gmail.com>
2018-03-21 22:31:01 -0400

    Tweak the responsiveness of images with Bootstrap and center captions for images

--- v1/app.js
+++ v1/app.js
@@ -5,6 +5,12 @@ var bodyParser = require('body-parser');
 var campgrounds = [
     {name: "Salmon Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN8t9341--AV174FR94sQ4kGlTQAnrah5iYvp-GAwl-hi5QEDigA"},
     {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_AwJqrTGL6IcA3w205uu9-X5I1BPCsCN4C-y53tREgH63PGrj"},
+    {name: "Mountain Goat's Rest", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWwpEGQZCO0xAqvT6EYdp1ji445g1aDWPuOyRgBRo-r36fDVUd"},
+    {name: "Salmon Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN8t9341--AV174FR94sQ4kGlTQAnrah5iYvp-GAwl-hi5QEDigA"},
+    {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs1hBvNnpg3oKhgvUapBBmbtW4Dq3oV2PrEGEt1xW_NJlpF5Xlyw"},
+    {name: "Mountain Goat's Rest", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWwpEGQZCO0xAqvT6EYdp1ji445g1aDWPuOyRgBRo-r36fDVUd"},
+    {name: "Salmon Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN8t9341--AV174FR94sQ4kGlTQAnrah5iYvp-GAwl-hi5QEDigA"},
+    {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_AwJqrTGL6IcA3w205uu9-X5I1BPCsCN4C-y53tREgH63PGrj"},
     {name: "Mountain Goat's Rest", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWwpEGQZCO0xAqvT6EYdp1ji445g1aDWPuOyRgBRo-r36fDVUd"}
 ]
 
